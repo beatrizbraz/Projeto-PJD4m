7d97514db6187a63d1fd0197ebcc4aa929e0d45c
beatrizbraz <beatriz.braz@escolar.ifrn.edu.br>
2023-04-11 20:25:20 -0300

    commit

---
+++ Projetos PJD4m/Assets/scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    
+    // Start is called before the first frame update
+   private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+   private void Start()
+   {
+       LoadScene("Splash");
+   }
+
+   public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
+
